994c8c58a37a4443bf132181383a8cda6033ec5b
noah.lovato <noragami212>
2017-06-27 08:17:32 -0700

    Initializing repo

---
+++ app/src/main/java/com/example/noahlovato/stressreliever/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.noahlovato.stressreliever;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
